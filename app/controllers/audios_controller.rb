372e1ba5a712d68e3c0b5159a154afb9bf94146d
Conner Van Loan <cdvanloan@gmail.com>
2023-06-05 13:29:04 -0600

    Adds only god Knows

--- app/controllers/audios_controller.rb
+++ app/controllers/audios_controller.rb
@@ -9,4 +9,9 @@ class AudiosController < ApplicationController
     audio_path = Rails.root.join('app', 'assets', 'audios', 'whats_the_name_chris.mp3')
     send_file audio_path, type: 'audio/mpeg', disposition: 'inline'
   end
+
+  def only_god_knows
+    audio_path = Rails.root.join('app', 'assets', 'audios', 'only_god_knows.mp3')
+    send_file audio_path, type: 'audio/mpeg', disposition: 'inline'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
 
   get '/play_audio', to: 'audios#play'
   get '/whats_the_name', to: 'audios#whats_the_name'
+  get '/only_god_knows', to: 'audios#only_god_knows'
 end
 
